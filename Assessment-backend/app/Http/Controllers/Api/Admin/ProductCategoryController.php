1e76a32bdadf37a696fdf57823164f8e79c7c850
kritisha <kritishamoktan530@gmail.com>
2024-02-26 19:43:13 +0545

    added product category

---
+++ Assessment-backend/app/Http/Controllers/Api/Admin/ProductCategoryController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductCategoryRequest;
+use App\Http\Requests\UpdateProductCategoryRequest;
+use App\Http\Resources\ProductCategoryResource;
+use App\Models\ProductCategory;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+
+class ProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * 
+     * @return AnonymousResourceCollection
+     */
+    public function index(): AnonymousResourceCollection
+    {
+        $productCategories = ProductCategory::all();
+
+        return ProductCategoryResource::collection($productCategories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * 
+     * @param StoreProductCategoryRequest $request
+     * @return ProductCategoryResource
+     */
+    public function store(StoreProductCategoryRequest $request): ProductCategoryResource
+    {
+        $data = $request->validated();
+        $productCategory = ProductCategory::create($data);
+
+        return new ProductCategoryResource($productCategory);
+    }
+
+    /**
+     * Display the specified resource.
+     * 
+     * @param ProductCategory $productCategory
+     * @return ProductCategoryResource
+     */
+    public function show(ProductCategory $productCategory): ProductCategoryResource
+    {
+        return new ProductCategoryResource($productCategory);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * 
+     * @param UpdateProductCategoryRequest $request
+     * @param ProductCategory $productCategory
+     * @return ProductCategoryResource
+     */
+    public function update(UpdateProductCategoryRequest $request, ProductCategory $productCategory): ProductCategoryResource
+    {
+        $data = $request->validated();
+        $productCategory->update($data);
+
+        return new ProductCategoryResource($productCategory);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * 
+     * @param ProductCategory $productCategory
+     * @return Response
+     */
+    public function destroy(ProductCategory $productCategory): Response
+    {
+        $productCategory->delete();
+
+        return response()->noContent();
+    }
+}
---
+++ Assessment-backend/app/Models/ProductCategory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductCategory extends Model
+{
+    use HasFactory, SoftDeletes;
+    
+    /**
+     * The table associated with the model.
+     * @var string
+     */
+    protected $table = 'product_categories';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+}
